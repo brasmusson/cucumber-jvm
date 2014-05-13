7160c373e3d8b9b084d01658a22df26a23193b78
Björn Rasmusson <B.Rasmusson@computer.org>
2014-05-13 20:18:14 +0200

    Added test for transactions and glue class injection to spring

--- spring/src/test/java/cucumber/runtime/java/spring/RunCukesTest.java
+++ spring/src/test/java/cucumber/runtime/java/spring/RunCukesTest.java
@@ -4,5 +4,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@Cucumber.Options(glue = {"cucumber.runtime.java.spring", "cucumber.api.spring"})
 public class RunCukesTest {
 }
